33714e71d5d9d245302aabef1e076df34f20caa6
apitoolkits <endro.mono@yahoo.co.id>
2017-11-08 15:05:40 +0700

    src up
    up

---
+++ src/Curl.php
@@ -0,0 +1,14 @@
+<?php namespace Apitoolkits\Curl;
+
+class Curl{
+	/**
+	 * @param $url string   The URL to which the request is to be sent
+	 * @return \Atk\Curl\Builder
+	 */
+	public function to($url)
+	{
+		$builder = new Builder();
+		
+		return $builder->to($url);
+	}
+}
